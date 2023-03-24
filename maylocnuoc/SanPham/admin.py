6c6142a4e26ac90addcd202ece1f8a81800eca01
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-24 11:18:11 +0700

    implement tiny mce

--- maylocnuoc/SanPham/admin.py
+++ maylocnuoc/SanPham/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django import forms
 from SanPham.models import SanPham
+from tinymce.widgets import TinyMCE
+
+class PersonForm(forms.ModelForm):
+    mo_ta_dai = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+    class Meta:
+        model = SanPham
+        fields = '__all__'
 
 class SanPhamAdmin(admin.ModelAdmin):
-    pass
+    form = PersonForm
 
 admin.site.register(SanPham, SanPhamAdmin)
--- maylocnuoc/SanPham/models.py
+++ maylocnuoc/SanPham/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from TheLoai.models import TheLoai
+from tinymce import models as tinymce_models
 
 class SanPham(models.Model):
     ten = models.CharField(max_length=500, null=False, blank=False)
@@ -8,4 +9,4 @@ class SanPham(models.Model):
     gia = models.FloatField(null=True, blank=True, default=0)
     don_vi_tien = models.CharField(max_length=10, null=True, blank=True, default="VND")
     mo_ta_ngan = models.TextField(null=True, blank=True)
-    mo_ta_dai = models.TextField(null=True, blank=True)
+    mo_ta_dai = tinymce_models.HTMLField(null=True, blank=True)
