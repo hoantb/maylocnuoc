eea4e05efed7b3d5befef0ece72b0f1eb09e3256
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-24 19:47:30 +0700

    update san pham noi bat

--- maylocnuoc/SanPham/admin.py
+++ maylocnuoc/SanPham/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django import forms
-from SanPham.models import SanPham
+from SanPham.models import SanPham, SanPhamNoiBat
 from tinymce.widgets import TinyMCE
 
-class PersonForm(forms.ModelForm):
+class SanPhamForm(forms.ModelForm):
     mo_ta_dai = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
     class Meta:
         model = SanPham
         fields = '__all__'
 
 class SanPhamAdmin(admin.ModelAdmin):
-    form = PersonForm
+    form = SanPhamForm
+
+class SanPhamNoiBatAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(SanPham, SanPhamAdmin)
+admin.site.register(SanPhamNoiBat, SanPhamNoiBatAdmin)
