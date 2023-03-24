938f734e0b26d92e38facc0f49315646c2861dda
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-24 22:32:03 +0700

    update pagination

--- maylocnuoc/SanPham/views.py
+++ maylocnuoc/SanPham/views.py
@@ -1,17 +1,36 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from SanPham.models import SanPham
 from django.shortcuts import get_object_or_404
 from SanPham.serializers import SanPhamSerializer
 from rest_framework.response import Response
+from rest_framework import pagination
+
+class SanPhamPagination(pagination.PageNumberPagination):
+    page_size = 9
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 
-class SanPhamViewSet(viewsets.ViewSet):
+
+class SanPhamViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """
     A simple ViewSet for listing or retrieving SanPham.
     """
+    serializer_class = SanPhamSerializer
+    pagination_class = SanPhamPagination
+
+    def get_queryset(self):
+        return SanPham.objects.all()
+
     def list(self, request):
         queryset = SanPham.objects.all()
-        serializer = SanPhamSerializer(queryset, many=True)
+        #serializer = SanPhamSerializer(queryset, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
