64ba6d4b68616fea83c2ee50e98877f077c8ce23
hoan bt <hoan.buithanh91@gmail.com>
2023-04-05 23:00:08 +0700

    update search

--- maylocnuoc/SanPham/views.py
+++ maylocnuoc/SanPham/views.py
@@ -28,7 +28,7 @@ class SanPhamViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         search_name = request.GET.get('search-name', None)
 
         if search_name:
-            queryset = queryset.filter(ten__contains=search_name)
+            queryset = queryset.filter(ten__icontains=search_name)
 
         if sort_type == "latest":
             queryset = queryset
