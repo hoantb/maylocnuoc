9baba6bca2925c874d91425406b5356d42e61e9d
hoan bt <hoan.buithanh91@gmail.com>
2023-04-11 16:24:50 +0700

    update giao dien admin

--- maylocnuoc/DichVu/admin.py
+++ maylocnuoc/DichVu/admin.py
@@ -4,12 +4,13 @@ from DichVu.models import DichVu
 from tinymce.widgets import TinyMCE
 
 class DichVuForm(forms.ModelForm):
+    mo_ta_ngan = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 50}))
     mo_ta_dai = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 100}))
     class Meta:
         model = DichVu
         fields = '__all__'
 
 class DichVuAdmin(admin.ModelAdmin):
-    pass
+    form = DichVuForm
 
 admin.site.register(DichVu, DichVuAdmin)
--- maylocnuoc/GioiThieu/admin.py
+++ maylocnuoc/GioiThieu/admin.py
@@ -4,12 +4,13 @@ from GioiThieu.models import GioiThieu
 from tinymce.widgets import TinyMCE
 
 class GioiThieuForm(forms.ModelForm):
+    mo_ta_ngan = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 50}))
     mo_ta_dai = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 100}))
     class Meta:
         model = GioiThieu
         fields = '__all__'
 
 class GioiThieuAdmin(admin.ModelAdmin):
-    pass
+    form = GioiThieuForm
 
 admin.site.register(GioiThieu, GioiThieuAdmin)
