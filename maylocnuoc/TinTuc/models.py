6d3ca6c8deb762a6beb3fb2362e7bca3c43dee8b
hoan bt <hoan.buithanh91@gmail.com>
2023-04-13 21:12:57 +0700

    update tin tuc

--- maylocnuoc/TinTuc/models.py
+++ maylocnuoc/TinTuc/models.py
@@ -3,5 +3,6 @@ from tinymce import models as tinymce_models
 
 class TinTuc(models.Model):
     tieu_de = models.CharField(max_length=1000, null=False, blank=False)
+    hinh_anh = models.ImageField(null=True, blank=True, upload_to='tin-tuc')
     mo_ta_ngan = models.TextField()
     mo_ta_dai = tinymce_models.HTMLField(null=True, blank=True)
