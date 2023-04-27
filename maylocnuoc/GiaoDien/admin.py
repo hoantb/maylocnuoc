9246064217a8122cc39918ad2d451ad072d0a5d5
hoan bt <hoan.buithanh91@gmail.com>
2023-04-27 21:42:29 +0700

    update admin

--- maylocnuoc/GiaoDien/admin.py
+++ maylocnuoc/GiaoDien/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from GiaoDien.models import SlideTrangChu
+from GiaoDien.models import SlideTrangChu, HinhTrangGioiThieu, HinhTrangLienHe
 
 class SlideTrangChuAdmin(admin.ModelAdmin):
     pass
 
+class HinhTrangGioiThieuAdmin(admin.ModelAdmin):
+    pass
+
+class HinhTrangLienHeAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(SlideTrangChu, SlideTrangChuAdmin)
+admin.site.register(HinhTrangGioiThieu, HinhTrangGioiThieuAdmin)
+admin.site.register(HinhTrangLienHe, HinhTrangLienHeAdmin)
